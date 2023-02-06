9fd0dfc459db00cff4cab60ee17a07cb57d1e266
Gurushesh <gurushesh.p@metapercept.com>
2023-02-06 10:59:51 +0530

    props passed

---
+++ src/views/Code.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="basic">
+        <div class="counter">
+            <button @click="descrement">-</button>
+            <p>{{ age }}</p>
+            <button @click="increment">+</button>
+        </div>
+
+        <div class="boxx">
+           
+            <label for="userName">userName :</label>
+            <input type="text" name="userName" v-on:input="nameInput('evemt is triggerd', $event)">
+           
+            <label for="password">Password :</label>
+            <input type="password" name="password" @keyup.enter="keyPressHandler">
+
+            <label for="age">age</label>
+            <input type="text" v-model.number.lazy.trim="age">
+            <span>
+                {{ userName }}
+            </span>
+
+            <span>
+                {{ password }}
+            </span>
+        </div>
+    </div>
+</template>
+
+<script>
+console.log("in code");
+export default {
+
+    // eslint-disable-next-line vue/multi-word-component-names
+    name : 'Code',
+    data() {
+        return {
+            age: 0,
+            userName : "",
+            password : ""
+        }
+    },
+    methods : {
+        descrement() {
+            this.age = this.age - 1
+        },
+        increment() {
+            this.age = this.age + 1;
+        },
+
+        nameInput(msg, e){
+            
+            console.log(msg);
+            console.log(e.target.value);
+            this.userName = e.target.value
+        },
+        keyPressHandler(e){
+            this.password = e.target.value
+            console.log(e.target.value);
+        }
+    }
+}
+</script>
+
+<style scoped>
+.counter {
+    margin-top: 4rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+button {
+    padding: 5px 1rem;
+}
+p {
+    margin: 0 1rem;
+}
+.boxx {
+    margin-top: 3rem;
+}
+
+span , input{
+    display: block;
+    margin: 0 auto;
+}
+
+</style>
